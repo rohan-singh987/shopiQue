5559c7e469552c77cf5b14d3760ad565c3ccd492
Rohan Singh <rohannsingh98765@gmail.com>
2022-07-14 20:19:31 +0530

    adding cart page and its logic
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,12 @@ import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
 import logo from '../assets/Shopping1.png'
 import Image from 'next/image'
+import Cart from './Cart'
+import { useStateContext } from '../context/StateContext'
 
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className=' navbar-container'>
         <p className='log'>
@@ -19,10 +22,12 @@ const Navbar = () => {
         </p>
             <button type='button' 
                 className=' cart-icon' 
-                onClick="">
+                onClick={() => setShowCart(true)}>
                 <AiOutlineShopping />
-                <span className='cart-item-qty'>1</span>
+                <span className='cart-item-qty'>{ totalQuantities }</span>
             </button>
+
+            { showCart && < Cart />}  
     </div>
   )
 }
